bdff32caf3e11cf87b0f0da8a1da77ebd6c04310
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-01-01 14:34:15 -0700

    finished chp 2

--- simpleFunctions.hs
+++ simpleFunctions.hs
@@ -1,3 +1,12 @@
 double x = x + x
+--reusing the functions 
 doubleTwo x y = double x + double y
-conditionalD x = if x < 100 then x*2 else x 
+conditionalD x = if x < 100 || x > 200 
+                  then x*2
+                  else x
+
+--recursive function
+myDrop n xs=  if n <=0 || null xs then xs else myDrop (n-1) (tail xs)
+
+--ex
+lastButOne lst= head(myDrop ((length lst)-2) lst) 
