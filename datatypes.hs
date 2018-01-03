f04dd5c324d419530805312c73d79cd84af96f01
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-01-02 21:50:44 -0700

    intro to data types

---
+++ datatypes.hs
@@ -0,0 +1,34 @@
+-- synonyms for redability 
+type Authors = [String]
+type ISSBN= Int
+
+-- similar to objects 
+data BookInfo = Book ISSBN String Authors
+                deriving (Show)
+data BookReview = Review String Authors
+                  deriving (Show)
+
+type BookRecord = (BookInfo, BookReview)
+
+-- instantiation 
+myInfo = Book 123 "Algebra of Programming"
+         ["Person 1", "Person 2"]
+myReview = Review "Super interesting approach to programming." ["Yonathan", "Fisseha"]
+
+-- multiple "constructors"
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+
+data BillingInfo = CreditCard CardNumber CardHolder Address
+                   | CashOnDelivery
+                   | Invoice Int
+                     deriving (Show)
+
+-- instantiation w/ multiple 'constructors'
+billingInfo1 = CreditCard "123 456" "Yonathan Fisseha" ["123 Main St."]
+billingInfo2 = CashOnDelivery
+billingInfo3 = Invoice 90
+
+
+
