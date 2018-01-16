2cc64b3741f4e13541a2b2463f382532022ded40
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-01-15 19:48:26 -0500

    new work on chapter 4. Didn't do practice problems

---
+++ chapter4.hs
@@ -0,0 +1,10 @@
+-- line split that works on window and *nix systems 
+splitLines [] = []
+splitLines cs =
+  let (pre, suf) = break isLineTerminator cs
+  in pre : case suf of
+             ('\r':'\n':rest) -> splitLines rest
+             ('\r':rest) -> splitLines rest
+             ('\n':rest) -> splitLines rest
+             _   -> []
+isLineTerminator c = c == '\r' || c == '\n'
